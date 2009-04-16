2da054bfbf75b06de9542e1ea95a65009a3a41af
Tiago Saboga <tiagosaboga@gmail.com>
2009-04-15 22:54:09 -0300

    Easier switching of function used to display buffer.

--- imp-buffers.el
+++ imp-buffers.el
@@ -1,27 +1,29 @@
 (defvar important-buffers '() )
-;; (setq ts-important-buffers '() )
-;; (setq debug-on-quit nil)
+
+;(defalias 'imp-show-buffer 'pop-to-buffer) ; other window, select it
+;(defalias 'imp-show-buffer 'display-buffer) ; other window, don't select
+(defalias 'imp-show-buffer 'switch-to-buffer) ; current window
 
 (defun mark-current-buffer-as-important ()
   "Toggle important mark on current buffer."
   (interactive)
   (let ((buffer (current-buffer)))
-    (if (memq buffer ts-important-buffers)
-        (setq ts-important-buffers (delq buffer ts-important-buffers))
-      (add-to-list 'ts-important-buffers buffer))))
+    (if (memq buffer important-buffers)
+        (setq important-buffers (delq buffer important-buffers))
+      (add-to-list 'important-buffers buffer))))
 
 (defun goto-next-important-buffer ()
   "Go to next buffer marked as important.
 TODO: cycle through all buffers, not only the first ones."
   (interactive)
-  (let (important-buffers)
+  (let (imp-buffers)
     (while important-buffers
       (let (nextbuffer)
         (setq nextbuffer (pop important-buffers))
-        (add-to-list 'important-buffers nextbuffer)
+        (add-to-list 'imp-buffers nextbuffer)
         (if (not (get-buffer-window nextbuffer))
             (progn
-              (pop-to-buffer nextbuffer)
+              (imp-show-buffer nextbuffer)
               (if important-buffers
                   (progn
                     (setq imp-buffers 
