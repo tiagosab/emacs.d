58fede4d7918280fce06623abeb45ae7de93f874
Tiago Saboga <tiagosaboga@gmail.com>
2011-05-18 12:35:11 -0300

    Working on org-mode/jekyll integration.

--- ts-org-mode-addons.el
+++ ts-org-mode-addons.el
@@ -1,5 +1,21 @@
 (require 'cl-macs)
 
+(setq org-publish-project-alist
+      '(
+        ("org-hack-blog"
+         ;; My hack blog
+         :base-directory "~/h/hack/"
+         :base-extension "org"
+         
+         ;; The jekyll project
+         :publishing-directory "~/h/blog"
+         :recursive t
+         :publishing-function org-publish-org-to-html
+         :headline-levels 4
+         :html-extension "html"
+         :body-only t  ;; Only export html body
+         )
+        (
 (defun ts-move-up-whole-row ()
   (interactive)
   (save-excursion
