dd0e096146fb1e855074e6245651730faa659ad2
Tiago Saboga <tiagosaboga@gmail.com>
2015-03-24 22:41:22 -0400

    Adapt packages loading to paradox

--- emacs.el.d/60-packages.el
+++ emacs.el.d/60-packages.el
@@ -2,8 +2,35 @@
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
-(setq package-load-list
-      '((key-chord t)))
+(setq ts-my-packages
+      '(adoc-mode
+        gh
+        gist
+        git-blame
+        key-chord
+        bts-github
+        )
+      )
+
+
+; It must exist a way to say to package-install/paradox to install
+; these packages and its dependencies. For now I am adding 'all,
+; but I'd rather not.
+
+(let ((packlist
+       (mapcar (lambda (value) "Doc" `(,value t)) ts-my-packages)))
+  (setq package-load-list (add-to-list 'packlist 'all t)))
+
 (package-initialize)
 (setq package-enable-at-startup nil)
 
+(if (not (require 'paradox "paradox" t))
+    (package-install 'paradox))
+
+(paradox-require 'adoc-mode)
+(paradox-require 'gh)
+(paradox-require 'gist)
+(paradox-require 'git-blame)
+(paradox-require 'key-chord)
+(paradox-require 'bts-github)
+(paradox-require 'adoc-mode)
